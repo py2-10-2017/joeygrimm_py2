886f69b124659c7fef3954375210dad951c02d90
joeygrimm <joeygrimm86@gmail.com>
2017-09-05 22:00:24 -0500

    content added

---
+++ week1/filter_by_type.py
@@ -0,0 +1,29 @@
+def Integar(a):
+    if a >= 100:
+        print "That's a big number!"
+    else:
+        print "That is a small number....:("
+        
+number = 101
+
+Integar(number)
+
+def String(a):
+    if len(a) >= 50:
+        print "That is a long sentence!"
+    else:
+        print "Short sentence"
+
+statement = "I was thinking of going to the beach later today. However I changed my mind and now I am going to watch Netflix and binge watch television"
+
+String(statement)
+
+def List(a):
+    if len(a) >= 10:
+        print "What a big list!!!"
+    else:
+        print "What a short list this is!"
+
+items = ["Tacos", "Burritos", "Chips", "Salsa", "Cheese", "Sour Cream", "Avocados", "Black Beans", "Corn", "Lettuce"]
+
+List(items)
