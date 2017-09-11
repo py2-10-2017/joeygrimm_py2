d0b2dfa270f0e5dcfdae64f091fce4978e01d60c
joeygrimm <joeygrimm86@gmail.com>
2017-09-11 09:39:09 -0500

    new content added

--- week1/Making_Dictionaries.py
+++ week1/Making_Dictionaries.py
@@ -1,8 +1,24 @@
 My_info = {"Name": "Joey", "Age": 31, "State": "Kansas", "Pet": "Yogi"}
 
+#This was my first the first function I made for this problem but after reviewing the solution code, I prefer using iteritems()
+
 def Dict_Info(dict):
     print "Hello my name is", dict["Name"], "and I am", dict["Age"], "years old!"
     print "I am from the state of", dict["State"], "and reside in the state of", dict["State"], "."
     print "I have a dog named", dict["Pet"], "."
 
 Dict_Info(My_info)
+
+
+def print_dictionary_values(dic):
+    for some_key, some_value in dic.iteritems():
+        if some_key is "Name":
+            print "Hello my {} is {}.".format(some_key, some_value)
+        elif some_key is "State":
+            print "I am from the {} of {}.".format(some_key, some_value)
+        elif some_key is "Age":
+            print "My {} is {} years old.".format(some_key, some_value)
+        else:
+            print "My {} is named {}.".format(some_key, some_value)
+   
+print_dictionary_values(My_info)
