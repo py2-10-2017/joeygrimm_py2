b668f3cec147a8beac0d91930efaa7b8ec383297
joeygrimm <joeygrimm86@gmail.com>
2017-09-10 21:34:42 -0500

    new content added

---
+++ week1/Coin_tosses.py
@@ -0,0 +1,19 @@
+'''Write a function that simulates tossing a coin 5,000 times. Your function should print how many times the head/tail appears.
+Use the python built-in round function to convert that floating point number into an integer'''
+import random
+
+def coin_tosses():
+    heads_count = 0
+    tails_count = 0
+    heads = 1
+    tails = 2
+    for i in range(0, 5000):
+        x = random.randint(1,2)
+        if x is heads:
+            heads_count += 1
+            print "This coin toss is heads. You have tossed " + str(heads_count) + " heads and " + str(tails_count)+ " tails."
+        else:
+            tails_count += 1
+            print "This coin toss is tails. You have tossed " + str(heads_count) + " heads and " + str(tails_count) + " tails."
+
+coin_tosses()
