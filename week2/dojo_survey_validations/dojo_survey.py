c47b8375ff25f4fea04177630f54706644941ead
joeygrimm <joeygrimm86@gmail.com>
2017-09-21 22:42:48 -0500

    Dojo Survey Validation update

--- week2/dojo_survey_validations/dojo_survey.py
+++ week2/dojo_survey_validations/dojo_survey.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
-
+app.secret_key = 'KeepItSecretKeepItSafe'
 @app.route('/')
 def index():
   return render_template("index.html", dojo = "Dojo Survey")
 
 @app.route('/results', methods=['POST'])
 def create_user():
-   
-   return render_template("results.html", user= request.form)
-app.run(debug=True) 
+
+    errors = False
+
+    if len(request.form['name']) < 1:
+       flash("Your name cannot be empty.")
+       errors = True
+
+    if len(request.form['email']) < 1:
+       flash("Your email cannot be empty.")
+       errors = True
+
+    if len(request.form['comment']) < 1:
+      flash("Please leave a comment.")
+      errors = True
+
+    if len(request.form['comment']) > 120:
+       flash("You've commented too much. Please make it less than 120 characters.")
+       errors = True
+
+    if errors:
+       return redirect('/')
+
+
+    return render_template("results.html", user= request.form)
+
+app.run(debug=True)
