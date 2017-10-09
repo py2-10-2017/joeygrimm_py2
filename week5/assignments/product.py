0e134e38241aa21a947d5147236d4de97579a47e
joeygrimm <joeygrimm86@gmail.com>
2017-10-09 12:21:24 -0500

    product assignment done.

--- week5/assignments/product.py
+++ week5/assignments/product.py
@@ -1,32 +1,52 @@
-'''
-Assignment: Product
-The owner of a store wants a program to track products. Create a product class to fill the following requirements.
+class Product(object):
+    def __init__(self, price, name, weight, brand, cost, status):
+        self.price = price
+        self.name = name
+        self.weight = weight
+        self.brand = brand
+        self.cost = self.price * self.weight
+        self.status = status
+        self.status = "For Sale"
+    def Sell(self):
+        self.status = "Sold"
+        return self
+    def AddTax(self):
+        tax = self.cost *.09
+        self.cost = self.cost + tax
+        if self.cost < 0:
+            self.cost = 0
+        return self
+    def Return(self):
+        print "If the return is defective, type 'defective'. If the product is in the box, type 'in the box'\n"
+        message = raw_input('product is: \n')
+        if message == "defective":
+            self.status = "defective"
+            self.cost = 0
+            print "Return allowed.\n"
+            return self
+        elif message == "in the box":
+            self.status ="For Sale"
+            discount= self.cost * .20
+            self.cost = self.cost - discount
+            print "Return allowed.\n"
+            return self
+        else:
+            print "Return not allowed.\n"
+            return self
+    def display_all(self):
+        print "Item Brand: {}".format(self.brand)
+        print "Item Name: {}".format(self.name)
+        print "Item Price per Pound: {}".format(self.price)
+        print "Item Weight in Pounds: {}".format(self.weight)
+        print "Item Cost: {}".format(self.cost)
+        print "Item Status: {}\n".format(self.status)
+        return self
 
-Product Class:
-Attributes:
+banana = Product(1, "Banana",2, "Aldi", 2, "For Sale")
+banana.AddTax().Sell().display_all()
 
-• Price
+apple = Product(2.85, "Apple", 3, "HyVee", 7,"For Sale")
+apple.display_all().AddTax().Sell().Return().display_all()
 
-• Item Name
-
-• Weight
-
-• Brand
-
-• Cost
-
-• Status: default "for sale"
-Methods:
-
-• Sell: changes status to "sold"
-
-• Add tax: takes tax as a decimal amount as a parameter and returns the price of the item including sales tax
-
-• Return: takes reason for return as a parameter and changes status accordingly. 
-If the item is being returned because it is defective change status to defective and change price to 0. 
-If it is being returned in the box, like new mark it as for sale. If the box has been opened set status to used and apply a 20% discount.
-
-• Display Info: show all product details.
-Every method that doesn't have to return something should return self so methods can be chained.
-'''
-
+carrot= Product(.55, "Carrot", 4, "Price Chopper", 2, "For Sale")
+carrot.display_all().AddTax().Sell().Return().AddTax().Sell().display_all()
